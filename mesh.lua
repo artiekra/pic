7759e62b813146e693ba90e424c1dfcf943588ed
Artemii Kravchuk <artemiykra@gmail.com>
2025-01-10 19:44:25 +0200

    nits

--- mesh.lua
+++ mesh.lua
@@ -2,14 +2,14 @@ local pic = require("/dynamic/pic/.lua")
 local inspect = require("/dynamic/inspect.lua")
 
 
-local mesh = pic.Mesh:new()
+local mesh1 = pic.Mesh:new()
 
-mesh:new_line({0, 0}, {50, 50}, 0xff0000ff)
-mesh:new_line({0, 0}, {-50, 50}, 0x00ff00ff)
-mesh:new_line({0, -10}, {0, 50}, 0x0000ffff)
+mesh1:new_line({0, 0}, {50, 50}, 0xff0000ff)
 
-print(inspect(mesh))
 
-meshes = {mesh:compile()}
+local mesh1_compiled = mesh1:compile()
 
+meshes = {mesh1_compiled}
+
+print(inspect(mesh1))
 print(inspect(meshes))
