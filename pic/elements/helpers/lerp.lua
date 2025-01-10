c8a5d13e73099df0153bc8c6d4880f1adc5040a8
Artemii Kravchuk <artemiykra@gmail.com>
2025-01-10 22:12:50 +0200

    add lerp helpers

---
+++ pic/elements/helpers/lerp.lua
@@ -0,0 +1,19 @@
+local lerp = {}
+
+
+--- Linear interpolation between two values
+-- Lerp between start and finish value given t
+-- @param start start value
+-- @param finish finish value
+-- @param t cooficient
+-- @return the result of the lerp function
+function lerp(start, finish, t)
+
+    -- ensure t is clamped between 0 and 1
+    t = math.max(0, math.min(1, t))
+    
+    return start + (finish - start) * t
+end
+
+
+return lerp
