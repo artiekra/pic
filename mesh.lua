c03995bf11a34d1f29376e14951888ce4ac4ead5
Artemii Kravchuk <artemiykra@gmail.com>
2025-01-10 21:42:01 +0200

    support for 0, 1 or 2 colors for Line object

--- mesh.lua
+++ mesh.lua
@@ -4,13 +4,20 @@ local inspect = require("/dynamic/inspect.lua")
 
 local mesh1 = pic.Mesh:new()
 
+-- Test basic Line creation
 mesh1:new_line({0, 0}, {50, 50}, 0xff0000ff)
+
+-- Test different Point types
 mesh1:new_line(pic.Point(-50, 0), pic.PointPolar(100, 1, 10), 0x00ff00ff)
 
+-- Test different number of colors being used
+mesh1:new_line({10, 0}, {60, 50})
+mesh1:new_line({20, 0}, {70, 50}, {0xff0000ff, 0x0000ffff})
 
+
+print(inspect(mesh1))
 local mesh1_compiled = mesh1:compile()
 
 meshes = {mesh1_compiled}
 
-print(inspect(mesh1))
 print(inspect(meshes))
--- pic/elements/line.lua
+++ pic/elements/line.lua
@@ -1,5 +1,5 @@
 -- Meta Line class
-Line = {point1 = {}, point2 = {}, outline = 0}
+Line = {point1 = {}, point2 = {}, colors = {}}
 Line.__index = Line
 
 
@@ -25,15 +25,15 @@ local color_helpers = relative_import("helpers/color.lua")
 -- A simple line that goes through two given points
 -- @param point1 first point
 -- @param point2 second point
--- @param outline line settings
+-- @param colors line colors
 -- @return Line object
-function Line:new(point1, point2, outline)
+function Line:new(point1, point2, colors)
 
   local object = setmetatable({}, self)
 
   object.point1 = point1
   object.point2 = point2
-  object.outline = outline
+  object.colors = colors
 
   return object
 end
@@ -46,11 +46,22 @@ function Line:compile()
   local point1 = vertex_helpers.get_vertex(self.point1)
   local point2 = vertex_helpers.get_vertex(self.point2)
 
-  local color = color_helpers.get_color(self.outline)
+  local colors = color_helpers.get_color(self.colors)
 
   local computed_vertexes = {point1, point2}
   local computed_segments = {{0, 1}}
-  local computed_colors = {color, color}
+
+  local computed_colors
+  if #colors == 0 then
+    computed_colors = {0xffffff00, 0xffffff00}
+  elseif #colors == 1 then
+    local color = colors[1]
+    computed_colors = {color, color}
+  elseif #colors == 2 then
+    computed_colors = colors
+  else
+    error("Line must have 2 or less colors specified")
+  end
 
   return {computed_vertexes, computed_segments, computed_colors}
 end
