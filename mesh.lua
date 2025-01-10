fe24844745c3b89f8b037f8805684f10f4fcb5dd
Artemii Kravchuk <artemiykra@gmail.com>
2025-01-10 23:07:53 +0200

    organise the test mesh

--- mesh.lua
+++ mesh.lua
@@ -5,22 +5,25 @@ local inspect = require("/dynamic/inspect.lua")
 local mesh1 = pic.Mesh:new()
 
 -- Test basic Line creation
-mesh1:new_line({0, 0}, {50, 50}, 0xff0000ff)
-
--- Test different Point types
-mesh1:new_line(pic.Point(-50, 0), pic.PointPolar(100, 1, 10), 0x00ff00ff)
+mesh1:new_line({-200, 200}, {-160, 160}, 0xff0000ff)
 
 -- Test different number of colors being used
-mesh1:new_line({10, 0}, {60, 50})
-mesh1:new_line({20, 0}, {70, 50}, {0xff0000ff, 0x0000ffff})
-mesh1:new_line({30, 0}, {80, 50}, {0x0000ffff, 0xff0000ff, 0xffff00ff})
+mesh1:new_line({-160, 200}, {-120, 160}, {0xff0000ff, 0x0000ffff})
+mesh1:new_line({-140, 200}, {-100, 160}, {0x0000ffff, 0xff0000ff, 0xffff00ff})
+
+-- Line should not be visible
+mesh1:new_line({-180, 200}, {-140, 160})
+mesh1:new_line({-180, 200}, {-160, 180}, 0xffffffff)  -- for reference
+
+-- Test different Point types
+mesh1:new_line(pic.Point(-110, 200), pic.PointPolar(180, 2.05, 10), 0x00ff00ff)
 
 -- Random line with lots of colors (and z-axis involved)
-mesh1:new_line({-100, -20}, {100, 0}, {0x0000ffff, 0xff0000ff,
+mesh1:new_line({-80, 200}, {-20, 200, -50}, {0x0000ffff, 0xff0000ff,
   0xffff00ff, 0x00ff00ff, 0xffffffff, 0xff00ffff})
 
 -- Test different Color types
-mesh1:new_line({40, 0}, {90, 50}, {pic.Color(0xff0000ff),
+mesh1:new_line({-80, 190}, {-20, 190, -25}, {pic.Color(0xff0000ff),
   pic.ColorRGB(0, 0, 255), pic.ColorRGBA(255, 255, 0, 50),
   pic.ColorHSV(180, 1, 0.5)})
 
