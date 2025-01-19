e08e6b40a69790f8da2b5790c3c235f30707c681
Artemii Kravchuk <artemiykra@gmail.com>
2025-01-19 21:24:42 +0200

    refactor move transform

--- pic/elements/transforms/move.lua
+++ pic/elements/transforms/move.lua
@@ -1,17 +1,49 @@
 local transform_move = {}
 
 
+-- Get current path for relative imports
+local current_file = ...
+
+
+-- Deduct the path of the library for relative imports
+-- (with support for going up the directory tree)
+local function relative_import(file)
+    local library_folder = current_file:match("(.+)/[^/]*$") .. "/"
+    
+    -- split both paths into segments
+    local base_segments = {}
+    for segment in library_folder:gmatch("[^/]+") do
+        table.insert(base_segments, segment)
+    end
+    
+    local file_segments = {}
+    for segment in file:gmatch("[^/]+") do
+        if segment == ".." then
+            -- remove last segment when we see ..
+            table.remove(base_segments)
+        else
+            table.insert(file_segments, segment)
+        end
+    end
+    
+    -- reconstruct the path
+    local final_path = table.concat(base_segments, "/") .. "/" ..
+      table.concat(file_segments, "/")
+    
+    return require("/" .. final_path)
+end
+
+
+local vertex_helpers = relative_import("../helpers/vertex.lua")
+
+
 --- Apply the move transform to the vertex.
 -- Moves the vertex by a certain offset
-local function apply_to_vertex(vertex, options)
+local function apply_to_vertex(vertex, point)
 
-  local offset_x = options[1] or 0
-  local offset_y = options[2] or 0
-  local offset_z = options[3] or 0
-
-  local new_x = vertex[1] + offset_x
-  local new_y = vertex[2] + offset_y
-  local new_z = vertex[3] + offset_z
+  local new_x = vertex[1] + point[1]
+  local new_y = vertex[2] + point[2]
+  local new_z = vertex[3] + point[3]
 
   return {new_x, new_y, new_z}
 end
@@ -25,7 +57,8 @@ function transform_move.apply(mesh, options)
 
   local new_vertexes = {}
   for _, vertex in ipairs(vertexes) do
-    local new_vertex = apply_to_vertex(vertex, options)
+    local point = vertex_helpers.compile(options[1])
+    local new_vertex = apply_to_vertex(vertex, point)
     table.insert(new_vertexes, new_vertex)
   end
 
